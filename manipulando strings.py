bf89672f3d48286138a4840de8f4a7e9e8edb0c0
Gisele C <giseledasilvac@gmail.com>
2024-04-08 16:23:04 -0300

    strings em python

---
+++ manipulando strings.py
@@ -0,0 +1,26 @@
+curso = "pYthon"
+
+print(curso.upper())
+print(curso.lower())
+print(curso.title())
+
+print("-------------------")
+
+curso = "  PythoN "
+
+print(curso.strip() + ".")
+print(curso.lstrip() + ".")
+print(curso.rstrip() + ".")
+
+print("-------------------")
+
+curso = "Python"
+
+print(curso.center(25) + ".")
+print(curso.center(9, '#'))
+print(curso.center(10, '-'))
+print(".".join(curso))
+print("_".join(curso))
+
+print("-------------------")
+
--- ola, mundo.py
+++ ola, mundo.py
@@ -1,6 +1,10 @@
 print("Hello world!")
 
 nome = input("informe seu nome: ")
-idade = input("informe sua idade: ")
+idade = 50
 
-print("olá {nome} tudo bem? sua idade é {idade}")
+print("olá %s tudo bem? sua idade é %d" % (nome,idade))
+print("olá {} tudo bem? sua idade é {}".format(nome,idade))
+
+print("olá {1} tudo bem? sua idade é {2}".format(nome,idade))
+print("sua idade é {2}, adorável {1}".format(nome,idade))
