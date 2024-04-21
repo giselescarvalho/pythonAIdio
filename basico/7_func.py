fd0c70d21aac9c122fc02fd79f085ee83d5e3bee
Gisele C <giseledasilvac@gmail.com>
2024-04-20 21:41:33 -0300

    funções variavel global e local

--- basico/7_func.py
+++ basico/7_func.py
@@ -63,7 +63,6 @@ def criar_carro(modelo, ano, placa, /, marca, motor, combustivel):
 criar_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")
 #criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  
 
-
 #### Keyword only #####
 print("-------------")
 def criar_carro(*, modelo, ano, placa, marca, motor, combustivel):
@@ -81,3 +80,36 @@ def criar_carro(modelo, ano, placa, /, *, marca, motor, combustivel):
 
 criar_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")
 #criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  
+
+#### Obj primeira classe #####
+print("-------------")
+
+def somar(a, b):
+    return a + b
+
+def subtrair(a,b):
+    return a - b
+
+def mult(a,b):
+    return a * b
+
+def exibir_resultado(a, b, funcao):
+    resultado = funcao(a, b)
+    print(f"O resultado da operação entre {a} e {b} = {resultado}")
+
+exibir_resultado(10, 100, somar)  
+exibir_resultado(10, 100, subtrair) 
+exibir_resultado(10, 100, mult)
+
+#### Variavel global e local #####
+print("-------------")
+
+salario = 2000
+
+def salario_bonus(bonus):
+    global salario
+    salario += bonus
+    return salario
+
+
+print(salario_bonus(500))
