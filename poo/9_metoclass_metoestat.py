1dbbf3f31849ea8f747c7b87e19561197bc7dbc6
Gisele C <giseledasilvac@gmail.com>
2024-05-05 20:00:28 -0300

    metodo estatico

--- poo/9_metoclass_metoestat.py
+++ poo/9_metoclass_metoestat.py
@@ -2,15 +2,23 @@ class Pessoa:
     def __init__(self, nome=None, idade=None):
         self.nome = nome
         self.idade = idade
-
+#classe - cls
     @classmethod
     def criar_apartir_datanasc(cls, ano, mes, dia, nome):
        #print(cls)
        idade = 2024 - ano
        return cls(nome, idade)
 
+#estatico
+    @staticmethod
+    def e_maioridade(idade):
+        return idade >= 18
+
 p = Pessoa().criar_apartir_datanasc(1996, 3, 21, "João")
 print(p.nome, p.idade)
 
+#metodo de classe: de fabrica - apontam para a classe
 
-#metodo de classe: de fabrica
+#metodo estatico:  utilitarias - nao pode acessar nem modificar o estado
+print(Pessoa.e_maioridade(16))
+print(Pessoa.e_maioridade(21))
